cee08844fb11c6a3d20792876afe70dca8a71a4d
Vivianne <viviannevilar@gmail.com>
2020-10-25 17:15:03 +0800

    users app, customuser, admin (for both users and lists)

--- groupProjectBackend/lists/admin.py
+++ groupProjectBackend/lists/admin.py
@@ -4,11 +4,12 @@ from .models import Collection, Item
 
 class CollectionAdmin(admin.ModelAdmin):
     model = Collection
-    list_display = ['title','id','user','archived']
+    list_display = ['title','id','user','archived','date_created','attribute1','attribute2', 'attribute3', 'attribute4','attribute5']
 
 class ItemAdmin(admin.ModelAdmin):
     model = Item
-    list_display = ['name','id','user', 'collection']
+    list_display = ['name','id','user','collection','date_created','attribute1','attribute2', 'attribute3', 'attribute4','attribute5']
 
 admin.site.register(Collection,CollectionAdmin)
 admin.site.register(Item,ItemAdmin)
+
