1d7309f6951239e8fb2fc3751eda75c716af4b7f
Vivianne <viviannevilar@gmail.com>
2020-10-26 12:18:34 +0800

    Removed comments from serialisers.py

---
+++ groupProjectBackend/lists/serialisers.py
@@ -0,0 +1,24 @@
+from rest_framework import serializers
+from .models import Collection, Item
+
+class CollectionSerialiser(serializers.ModelSerializer):
+    user = serializers.ReadOnlyField(source='user.username')
+    date_created = serializers.ReadOnlyField()
+
+    class Meta:
+        model = Collection
+        fields = '__all__'
+
+class ItemSerialiser(serializers.ModelSerializer):
+    user = serializers.ReadOnlyField(source='user.username')
+    date_created = serializers.ReadOnlyField()
+    
+    class Meta:
+        model = Item
+        fields = '__all__'
+
+
+class CollectionDetailSerialiser(CollectionSerialiser):
+    collection_items = ItemSerialiser(many=True, read_only=True)
+
+
